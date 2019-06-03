ed2c462f26feff5ec206dfdb4e58b65b59c9c596
Vu The Huy <vuthehuyht@gmail.com>
2019-06-03 15:37:18 +0700

    first commit

---
+++ src/ProductionRule.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class ProductionRule {
+    ArrayList<String> N;
+    ArrayList<String> V;
+
+    public ProductionRule(ArrayList<String> N, ArrayList<String> V){
+        this.N = N;
+        this.V = V;
+    }
+
+    public String toString() {
+        String str = "";
+        for (String s : N) {
+            str += s + " ";
+        }
+        str += "->";
+        for (String s : V) {
+            str += " " + s;
+        }
+        return str;
+    }
+}
